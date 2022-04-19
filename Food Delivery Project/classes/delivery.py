ae207313bbdcb3c318bbc45a456f8442b5d24f70
XerckM <mercadoxerckiem@gmail.com>
2022-04-19 17:16:53 -0400

    added functions

--- Food Delivery Project/classes/delivery.py
+++ Food Delivery Project/classes/delivery.py
@@ -6,6 +6,8 @@ import mysql.connector
 class Delivery(object):
     def __init__(self):
         self.root = Tk()
+        # self.root.overrideredirect(True)
+        # self.root.attributes('-fullscreen', True)
         self.user = StringVar()
         self.pwd = StringVar()
         Login(self.root, self.user, self.pwd)
@@ -15,14 +17,14 @@ class Delivery(object):
     @staticmethod
     def output_frame(root):
         # set frame width and height
-        rframe_width = 1080
-        rframe_height = 720
+        dframe_width = 960
+        dframe_height = 540
 
         # calculate x and y coordinates for window position in screen
-        screen_pos_x = (root.winfo_screenwidth() / 2) - (rframe_width / 2)
-        screen_pos_y = (root.winfo_screenheight() / 2) - (rframe_height / 2)
+        screen_pos_x = (root.winfo_screenwidth() / 2) - (dframe_width / 2)
+        screen_pos_y = (root.winfo_screenheight() / 2) - (dframe_height / 2)
 
         # frame title, color, and size
         root.title("Food Delivery")
         root.configure(bg='gray12')
-        root.geometry('%dx%d+%d+%d' % (rframe_width, rframe_height, screen_pos_x, screen_pos_y))
+        root.geometry('%dx%d+%d+%d' % (dframe_width, dframe_height, screen_pos_x, screen_pos_y))
