6b380375a817a8db342cc6143179268604a54ae4
XerckM <mercadoxerckiem@gmail.com>
2022-04-19 08:44:40 -0400

    small changes

--- Food Delivery Project/classes/delivery.py
+++ Food Delivery Project/classes/delivery.py
@@ -14,11 +14,15 @@ class Delivery(object):
 
     @staticmethod
     def output_frame(root):
-        # Main window settings
+        # set frame width and height
         rframe_width = 1080
         rframe_height = 720
+
+        # calculate x and y coordinates for window position in screen
         screen_pos_x = (root.winfo_screenwidth() / 2) - (rframe_width / 2)
         screen_pos_y = (root.winfo_screenheight() / 2) - (rframe_height / 2)
+
+        # frame title, color, and size
         root.title("Food Delivery")
         root.configure(bg='gray12')
         root.geometry('%dx%d+%d+%d' % (rframe_width, rframe_height, screen_pos_x, screen_pos_y))
