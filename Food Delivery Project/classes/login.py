13a01f33ac3e57f1f88b239db12d793329ab889d
Zeke <67399106+XerckM@users.noreply.github.com>
2022-04-22 04:06:45 -0400

    edited file

--- Food Delivery Project/classes/login.py
+++ Food Delivery Project/classes/login.py
@@ -1,12 +1,13 @@
 from tkinter import *
 from tkinter import messagebox
 from PIL import Image, ImageTk
+from classes.admin import Admin
 import mysql.connector
 
 
 class Login(object):
     def __init__(self, root):
-        root.withdraw()
+        self.root = root
         self.user = StringVar()
         self.pwd = StringVar()
         self.login_frame(root)
@@ -16,10 +17,8 @@ class Login(object):
         Creates the login window for database login
         """
 
-        # global variables for login_connect
-        global user_entry
-        global pwd_entry
-        global frame
+        # global variables
+        global user_entry, pwd_entry, frame, log_button
 
         # top level frame and window icon
         frame = Toplevel(root)
@@ -51,14 +50,14 @@ class Login(object):
         logo.place(relx=0.5, rely=0.2, anchor=CENTER)
 
         # username entry box
-        user_entry = Entry(rlbl_frame, textvariable=self.user, fg='grey', relief='solid')
+        user_entry = Entry(rlbl_frame, textvariable=self.user, fg='grey', borderwidth=2)
         user_entry.insert(0, "Username")
         user_entry.place(relx=0.5, rely=0.45, anchor=CENTER)
         user_entry.bind('<FocusIn>', self.user_entry_onclick)
         user_entry.bind('<FocusOut>', self.user_entry_focusout)
 
         # password entry box
-        pwd_entry = Entry(rlbl_frame, textvariable=self.pwd, fg='grey', relief='solid')
+        pwd_entry = Entry(rlbl_frame, textvariable=self.pwd, fg='grey', borderwidth=2)
         pwd_entry.insert(0, 'Password')
         pwd_entry.place(relx=0.5, rely=0.55, anchor=CENTER)
         pwd_entry.bind('<FocusIn>', self.pwd_entry_onclick)
@@ -69,11 +68,18 @@ class Login(object):
         log_button.config(text='Login', width=10, height=1, relief='flat', bg='green', fg='white')
         log_button.place(relx=0.5, rely=0.7, anchor=CENTER)
 
+        close = Label(frame, text='Cancel', cursor='hand2', bg='white')
+        close.config(fg='blue')
+        close.place(relx=0.5, rely=0.701, anchor=CENTER)
+        close.bind('<Button-1>', lambda event: self.on_clickx(event))
+
         # Copyright label
         cr = Label(frame, text='Copyright © 2022 FoodHub Inc.', bg='#FF8000', fg='white')
-        cr.place(relx=0.5, rely=0.93, anchor=CENTER)
+        cr.place(relx=0.5, rely=0.94, anchor=CENTER)
 
         frame.bind('<Return>', lambda event: self.login_connect(event))
+        frame.protocol('WM_DELETE_WINDOW', self.on_clickx)
+        # frame.overrideredirect(True)
 
     def login_connect(self, event=None):
         """
@@ -85,17 +91,17 @@ class Login(object):
                                              passwd='password',
                                              database='food_delivery')
             uname = user_entry.get()
-            password = pwd_entry.get()
+            pword = pwd_entry.get()
             sql_cursor = sql_db.cursor()
             user_query = "SELECT * FROM users WHERE Uname = %s and Pword = %s"
-            sql_cursor.execute(user_query, [uname, password])
+            sql_cursor.execute(user_query, [uname, pword])
             result = sql_cursor.fetchall()
             user_level = str([res[0] for res in result]).strip('[]')
             if result:
                 messagebox.showinfo("", "Login Successful!")
                 frame.destroy()
                 if user_level == '1':
-                    print("Yes")
+                    return Admin(self.root, uname, pword)
                 else:
                     print("No")
             else:
@@ -129,3 +135,7 @@ class Login(object):
         if user_entry.get() == '':
             user_entry.insert(0, 'Password')
             user_entry.config(fg='grey')
+
+    def on_clickx(self, even=None):
+        if messagebox.askokcancel("Quit", "Are you sure you want to quit?"):
+            self.root.destroy()
