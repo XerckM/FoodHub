98514dfc53456c4c998641f547a09fa8138a2b1c
Zeke <67399106+XerckM@users.noreply.github.com>
2022-04-22 01:20:24 -0400

    removed lines

--- Food Delivery Project/main.py
+++ Food Delivery Project/main.py
@@ -3,10 +3,9 @@ from classes.login import Login
 
 
 def main():
+
     root = Tk()
-    user = StringVar()
-    pword = StringVar()
-    Login(root, user, pword)
+    Login(root)
     root.mainloop()
 
 
