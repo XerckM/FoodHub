3acd275103de017c4cdcb9a686c280fb1e54b7f8
Zeke <67399106+XerckM@users.noreply.github.com>
2022-05-05 04:53:07 -0400

    added admin frame and centered on screen

--- Food Delivery Project/classes/admin.py
+++ Food Delivery Project/classes/admin.py
@@ -1,4 +1,5 @@
 from tkinter import *
+from tkinter import messagebox
 import mysql.connector
 
 
@@ -8,9 +9,33 @@ class Admin(object):
         self.admin_frame(self.root)
 
     def admin_frame(self, root):
+        # database connector
         sql_db = mysql.connector.connect(host='localhost',
                                          port='3307',
                                          user='root',
                                          passwd='',
                                          database='food_delivery')
+
+        # top level frame and window icon
         frame = Toplevel(root)
+        frame.iconbitmap('win_ico.ico')
+
+        # set frame width and height
+        frame_width = 1080
+        frame_height = 720
+
+        # calculate x and y coordinates for window position in screen
+        screen_pos_x = (frame.winfo_screenwidth() / 2) - (frame_width / 2)
+        screen_pos_y = (frame.winfo_screenheight() / 2) - (frame_height / 2)
+
+        # frame title, color, and size
+        frame.title("FoodHub")
+        frame.resizable(False, False)
+        frame.configure(bg='white')
+        frame.geometry('%dx%d+%d+%d' % (frame_width, frame_height, screen_pos_x, screen_pos_y))
+
+        frame.protocol('WM_DELETE_WINDOW', self.on_clickx)
+
+    def on_clickx(self, event=None):
+        if messagebox.askokcancel("Quit", "Are you sure you want to quit?"):
+            self.root.destroy()
